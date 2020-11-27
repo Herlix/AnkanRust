186ff738b9473f760e6e134356f6526c17016fce
Alexander Herlin <alexander.herlin@outlook.com>
2020-11-27 15:53:26 +0100

    Back to trunk for simplicity

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 #![recursion_limit = "1024"]
-use wasm_bindgen::prelude::*;
-
 mod app;
 mod components;
 mod pages;
 mod slides_data;
 mod switch;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
+#[cfg(not(debug_assertions))]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // This is the entry point for the web app
-#[wasm_bindgen(start)]
 pub fn main() {
-    #[cfg(feature = "console_error_panic_hook")]
+    #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
     yew::start_app::<app::AppModel>();
